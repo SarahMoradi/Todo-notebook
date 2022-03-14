a8a5df2c10609164bfbbb41b4d5f88afbd187fe3
Sarah Moradi <moradisarah2001@gamil.com>
2022-03-14 17:34:07 +0330

    Added Edit option to Todo-App

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,17 +1,20 @@
 import Todo from './Todo'
+import TodoForm from './TodoForm';
+import { useState } from 'react';
 
-const TodoList = ({ todos, onComplete ,onDelete }) => {
+const TodoList = ({ todos, onComplete ,onDelete}) => {
 
+    const [edit, setEdit] = useState({id: null, text:"", isCompleted: false})
   const renderedTodos = () => {
     if (todos.length === 0) return <p>Add some Todos</p>;
     return todos.map((todo) => {
       return (
-        <Todo todo={todo} key={todo.id} onComplete={()=> onComplete(todo.id)} onDelete={()=>onDelete(todo.id)} />
+        <Todo todo={todo} key={todo.id} onComplete={()=> onComplete(todo.id)} onDelete={()=>onDelete(todo.id)} onEdit={()=>setEdit(todo)} />
       );
     });
   };
 
-  return <div>{renderedTodos()}</div>;
+  return <div>{edit.id ? <TodoForm/> : renderedTodos()}</div>;
 };
 
 export default TodoList;
